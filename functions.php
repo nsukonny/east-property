2712540dc651eb9e3c4e20171aeb2d3eb1fc8916
ns <NSukonny@gmail.com>
2026-07-26 16:53:40 +0300

    filemtime for styles cache

--- functions.php
+++ functions.php
@@ -49,7 +49,7 @@ function add_theme_styles(): void {
 		THEME_NAME . '-style',
 		THEME_URL . '/assets/css/styles.min.css',
 		null,
-		THEME_VERSION,
+		filemtime( THEME_URL . '/assets/css/styles.min.css' ),
 		false
 	);
 
@@ -59,7 +59,7 @@ function add_theme_styles(): void {
 		THEME_NAME . '-app',
 		THEME_URL . '/assets/js/main.min.js',
 		null,
-		THEME_VERSION,
+		filemtime( THEME_URL . '/assets/js/main.min.js' ),
 		true
 	);
 
