42013c010992533c58942a47bf2fed63ed2dde2f
ns <NSukonny@gmail.com>
2026-09-11 11:54:47 +0300

    upd submodules

--- core/includes/localization.php
+++ core/includes/localization.php
@@ -11,8 +11,6 @@
 function switch_to_lang( string $locale ): void {
 	$parts = preg_split( '/[-_]/', $locale );
 
-	//new line
-
 	if ( count( $parts ) >= 2 ) {
 		$locale = strtolower( $parts[0] ) . '_' . strtoupper( $parts[1] );
 	}
