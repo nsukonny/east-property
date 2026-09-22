c7de675fbf3512715a50233e5afa97f95958b600
ns <NSukonny@gmail.com>
2026-09-22 17:39:09 +0300

    projects by districts labels

--- core/components/sections/explore-by-beds.php
+++ core/components/sections/explore-by-beds.php
@@ -3,35 +3,43 @@
  * Section for display count of properties by bedroom
  */
 
-$units_count = get_units_count_by_bedrooms( 'off-plan' );
+$units_count = get_units_count_by_bedrooms( 'secondary' );
 
-$cards   = array();
-$cards[] = array(
+$cards       = array();
+$first_count = ( $units_count[0] ?? 0 ) + ( $units_count[1] ?? 0 );
+$cards[]     = array(
 	'title' => __( 'Studio or 1 bedroom', 'east-property' ),
-	'href'  => core_home_url( '/off-plan/?beds=studio,1' ),
-	'count' => ( $units_count[0] ?? 0 ) + ( $units_count[1] ?? 0 ),
+	'href'  => core_home_url( '/secondary/?beds=studio,1' ),
+	'count' => $first_count,
 	'image' => THEME_URL . '/assets/img/beds_10.png',
+	'label' => ( 1 < $first_count ) ? __( 'Properties', 'east-property' ) : __( 'Property', 'east-property' ),
 );
 
 $cards[] = array(
 	'title' => __( '2 bedrooms', 'east-property' ),
-	'href'  => core_home_url( '/off-plan/?beds=2' ),
+	'href'  => core_home_url( '/secondary/?beds=2' ),
 	'count' => $units_count[2] ?? 0,
 	'image' => THEME_URL . '/assets/img/beds_20.png',
+	'label' => ( 1 < ( $units_count[2] ?? 0 ) ) ? __( 'Properties', 'east-property' ) : __( 'Property',
+		'east-property' ),
 );
 
 $cards[] = array(
 	'title' => __( '3 bedrooms', 'east-property' ),
-	'href'  => core_home_url( '/off-plan/?beds=3' ),
+	'href'  => core_home_url( '/secondary/?beds=3' ),
 	'count' => $units_count[3] ?? 0,
 	'image' => THEME_URL . '/assets/img/beds_30.png',
+	'label' => ( 1 < ( $units_count[3] ?? 0 ) ) ? __( 'Properties', 'east-property' ) : __( 'Property',
+		'east-property' ),
 );
 
-$cards[] = array(
+$last_count = ( $units_count[4] ?? 0 ) + ( $units_count[5] ?? 0 ) + ( $units_count[6] ?? 0 );
+$cards[]    = array(
 	'title' => __( '3+ bedrooms', 'east-property' ),
-	'href'  => core_home_url( '/off-plan/?beds=4,5,6' ),
-	'count' => ( $units_count[4] ?? 0 ) + ( $units_count[5] ?? 0 ) + ( $units_count[6] ?? 0 ),
+	'href'  => core_home_url( '/secondary/?beds=4,5,6' ),
+	'count' => $last_count,
 	'image' => THEME_URL . '/assets/img/beds_40.png',
+	'label' => ( 1 < $last_count ) ? __( 'Properties', 'east-property' ) : __( 'Property', 'east-property' ),
 );
 
 get_component_template(
@@ -40,7 +48,7 @@ get_component_template(
 		'top_title' => array(
 			'h2'   => __( 'Explore properties by number of bedrooms', 'east-property' ),
 			'desc' => __( 'Quick picks for you in UAE', 'east-property' ),
-			'href' => core_home_url( '/off-plan/' ),
+			'href' => core_home_url( '/secondary/' ),
 			'link' => __( 'All Properties', 'east-property' ),
 		),
 		'cards'     => $cards,
--- core/template-parts/sections/explore.php
+++ core/template-parts/sections/explore.php
@@ -4,14 +4,15 @@ $top_title = $args['top_title'] ?? array();
 $cards     = $args['cards'] ?? array();
 
 ?>
-    <section class="categories">
-        <div class="container">
-            <div class="categories-wrapper">
+	<section class="categories">
+		<div class="container">
+			<div class="categories-wrapper">
 				<?php
 				if ( ! empty( $top_title ) ) {
-					get_template_part( 'core/components/titles/top-title', null,
+					get_template_part( 'core/components/titles/top-title',
+						null,
 						array(
-							'h2'   => $top_title['h2'] ?? '',
+							'h2' => $top_title['h2'] ?? '',
 							'desc' => $top_title['desc'] ?? '',
 							'href' => $top_title['href'] ?? '',
 							'link' => $top_title['link'] ?? '',
@@ -21,24 +22,24 @@ $cards     = $args['cards'] ?? array();
 
 				if ( ! empty( $cards ) ) {
 					?>
-                    <div class="categories-cards">
+					<div class="categories-cards">
 						<?php foreach ( $cards as $card ) { ?>
-                            <a href="<?php echo esc_url( $card['href'] ); ?>" class="category-card">
-                                <img class="category-card-bg" src="<?php echo esc_url( $card['image'] ); ?>" width="270"
-                                     height="270" alt="<?php echo esc_html( $card['title'] ); ?>">
-                                <div class="category-card-desc">
-                                    <h3>
+							<a href="<?php echo esc_url( $card['href'] ); ?>" class="category-card">
+								<img class="category-card-bg" src="<?php echo esc_url( $card['image'] ); ?>" width="270"
+									 height="270" alt="<?php echo esc_html( $card['title'] ); ?>">
+								<div class="category-card-desc">
+									<h3>
 										<?php echo esc_html( $card['title'] ); ?>
-                                    </h3>
-                                    <p><?php echo esc_attr( $card['count'] ); ?>
-										<?php esc_html_e( 'Properties' , 'east-property' ); ?>
-                                    </p>
-                                </div>
-                            </a>
+									</h3>
+									<p><?php echo esc_attr( $card['count'] ); ?>
+										<?php echo esc_html( $card['label'] ?? '' ); ?>
+									</p>
+								</div>
+							</a>
 						<?php } ?>
-                    </div>
+					</div>
 				<?php } ?>
-            </div>
-        </div>
-    </section>
+			</div>
+		</div>
+	</section>
 <?php
